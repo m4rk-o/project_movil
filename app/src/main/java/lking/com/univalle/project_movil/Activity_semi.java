f18eea6aad1b573f220e8e1bf7d2dc2a12d305ff
Jaskryt <jurassicandycraft@gmail.com>
2019-06-22 00:25:38 -0400

    Diseñador Andy Parte - 1

--- app/src/main/java/lking/com/univalle/project_movil/Activity_semi.java
+++ app/src/main/java/lking/com/univalle/project_movil/Activity_semi.java
@@ -6,9 +6,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class Activity_semi extends AppCompatActivity {
-    Button fib,pri,fac,mul,mod,ex,cri,mcd;
+    LinearLayout fib,pri,fac,mul,mod,ex,cri,mcd;
+    TextView fib1,pri1,fac1,mul1,mod1,ex1,cri1,mcd1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,81 +20,89 @@ public class Activity_semi extends AppCompatActivity {
         String titulo = recibir.getStringExtra("titulo");
         setTitle(titulo);
         fib = findViewById(R.id.btnFib);
+        fib1 = findViewById(R.id.textView1);
         fib.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent i = new Intent(v.getContext(),Activity_final.class);
-                String titulo = fib.getText().toString();
+                String titulo = fib1.getText().toString();
                 i.putExtra("titulo",titulo);
                 startActivity(i);
             }
         });
         pri = findViewById(R.id.btnPrimos);
+        pri1 = findViewById(R.id.textView3);
         pri.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent i = new Intent(v.getContext(),Activity_final.class);
-                String titulo = pri.getText().toString();
+                String titulo = pri1.getText().toString();
                 i.putExtra("titulo",titulo);
                 startActivity(i);
             }
         });
         fac = findViewById(R.id.btnFac);
+        fac1 = findViewById(R.id.textView5);
         fac.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent i = new Intent(v.getContext(),Activity_final.class);
-                String titulo = fac.getText().toString();
+                String titulo = fac1.getText().toString();
                 i.putExtra("titulo",titulo);
                 startActivity(i);
             }
         });
         mul = findViewById(R.id.btnMul);
+        mul1 = findViewById(R.id.textView7);
         mul.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent i = new Intent(v.getContext(),Activity_final.class);
-                String titulo = mul.getText().toString();
+                String titulo = mul1.getText().toString();
                 i.putExtra("titulo",titulo);
                 startActivity(i);
             }
         });
         mod = findViewById(R.id.btnMod);
+        mod1 = findViewById(R.id.textView9);
         mod.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent i = new Intent(v.getContext(),Activity_final.class);
-                String titulo = mod.getText().toString();
+                String titulo = mod1.getText().toString();
                 i.putExtra("titulo",titulo);
                 startActivity(i);
             }
         });
         ex = findViewById(R.id.btnExp);
+        ex1 = findViewById(R.id.textView11);
         ex.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent i = new Intent(v.getContext(),Activity_final.class);
-                String titulo = ex.getText().toString();
+                String titulo = ex1.getText().toString();
                 i.putExtra("titulo",titulo);
                 startActivity(i);
             }
         });
         cri = findViewById(R.id.btnCri);
+        cri1 = findViewById(R.id.textView13);
         cri.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent i = new Intent(v.getContext(),Activity_final.class);
-                String titulo = cri.getText().toString();
+                String titulo = cri1.getText().toString();
                 i.putExtra("titulo",titulo);
                 startActivity(i);
             }
         });
         mcd = findViewById(R.id.btnMcd);
+        mod1 = findViewById(R.id.textView15);
         mcd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent i = new Intent(v.getContext(),Activity_final.class);
-                String titulo = mcd.getText().toString();
+                String titulo = mcd1.getText().toString();
                 i.putExtra("titulo",titulo);
                 startActivity(i);
             }
--- app/src/main/java/lking/com/univalle/project_movil/MainActivity.java
+++ app/src/main/java/lking/com/univalle/project_movil/MainActivity.java
@@ -5,41 +5,47 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    Button num,cad,sin;
+    ImageView num,cad,sin;
+    TextView num1,cad1,sin1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         num = findViewById(R.id.btnnum);
+        num1 = findViewById(R.id.textView1);
         num.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent i = new Intent(v.getContext(),Activity_semi.class);
-                String titulo = num.getText().toString();
+                String titulo = num1.getText().toString();
                 i.putExtra("titulo",titulo);
                 i.putExtra("con","num");
                 startActivity(i);
             }
         });
         cad = findViewById(R.id.btnstr);
+        cad1 = findViewById(R.id.textView2);
         cad.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent i = new Intent(v.getContext(),Activity_semi_strings.class);
-                String titulo = cad.getText().toString();
+                String titulo = cad1.getText().toString();
                 i.putExtra("titulo",titulo);
                 i.putExtra("con","cad");
                 startActivity(i);
             }
         });
         sin = findViewById(R.id.btnsint);
+        sin1 = findViewById(R.id.textView3);
         sin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent i = new Intent(v.getContext(),Activity_semi_sintaxis.class);
-                String titulo = sin.getText().toString();
+                String titulo = sin1.getText().toString();
                 i.putExtra("titulo",titulo);
                 i.putExtra("con","sin");
                 startActivity(i);
