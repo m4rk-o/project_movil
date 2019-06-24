073808d72fd5eaf705183555306b1c80a740c835
Marco <asfasf@gmail.com>
2019-06-24 05:12:25 -0400

    Segundo commit marko...

---
+++ app/src/main/java/lking/com/univalle/project_movil/Activity_c_criba.java
@@ -0,0 +1,13 @@
+package lking.com.univalle.project_movil;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Activity_c_criba extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_c_criba);
+    }
+}
